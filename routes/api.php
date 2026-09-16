0b748d2e5a91fb16e37d332ef0ae84b220a5b556
ZamiFaiz24 <faizzamzami10p@gmail.com>
2026-09-16 11:47:11 +0700

    add: Import Route ProductController and Route SupplierController in api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Inventory\CategoryController;
+use App\Http\Controllers\Inventory\ProductController;
+use App\Http\Controllers\Inventory\SupplierController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('categories', CategoryController::class);
+
+Route::apiResource('products', ProductController::class);
+
+Route::apiResource('suppliers', SupplierController::class);
